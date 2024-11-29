441b715f9d53520d2eb75a393e6e4c4a36dbaac4
Bhimsingh123 <sainibhimsingh2001@gmail.com>
2024-11-29 22:48:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,11 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 //Font Awesome
 import "./assets/icons/fontawesome/css/fontawesome.min.css";
 import "./assets/icons/fontawesome/css/all.min.css";
-
 //Box icon
 import "./assets/icons/boxicons/css/boxicons.min.css";
 import "./index.css";
-
 //CSS
 import "./assets/css/style.css";
-
 // Feather
 import "./assets/icons/feather/css/iconfont.css"
 
